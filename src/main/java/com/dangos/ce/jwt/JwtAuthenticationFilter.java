b56fdb212a6e22f5fabd91d8090186d6876935f8
chrisiogwaan <chris322322@gmail.com>
2024-08-01 15:43:09 +1000

    Code review - remove unused code

--- src/main/java/com/dangos/ce/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/dangos/ce/jwt/JwtAuthenticationFilter.java
@@ -4,7 +4,6 @@ import com.dangos.ce.util.R;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.annotation.Nonnull;
 import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
