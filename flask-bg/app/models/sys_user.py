935877faa5e50d9799e7654d3d4d7d189fd4bd66
Weixi (Chrisio) Guan <chris322322@gmail.com>
2025-05-06 14:21:29 +0800

    +refactor: Update SysUser model fields for consistency and improved data handling

--- flask-bg/app/models/sys_user.py
+++ flask-bg/app/models/sys_user.py
@@ -8,42 +8,45 @@ class SysUser(db.Model):
     __tablename__ = "sys_user"
 
     # UUID primary key
-    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    id = db.Column(db.String(256), primary_key=True, default=lambda: str(uuid.uuid4()))
+    
+    # User's avatar file ID
+    avatar_file_id = db.Column(db.BigInteger, nullable=True)
 
     # Username
-    username = db.Column(db.String(50), nullable=False, unique=True)
+    username = db.Column(db.String(64), nullable=False, unique=True)
 
     # User's first name
-    firstname = db.Column(db.String(50))
+    firstname = db.Column(db.String(64), nullable=True)
 
     # User's last name
-    lastname = db.Column(db.String(50))
+    lastname = db.Column(db.String(64), nullable=True)
 
     # User's password
-    password = db.Column(db.String(128), nullable=False)
+    password = db.Column(db.String(64), nullable=False)
 
     # User's email
-    email = db.Column(db.String(120), nullable=False, unique=True)
+    email = db.Column(db.String(120), nullable=True, unique=True)
 
-    # User's role
-    role = db.Column(db.Enum(Role), nullable=False, default=Role.USER)
-
-    # User's token
-    token = db.Column(db.String(255))
+    # User's role Big Id
+    sys_role_id = db.Column(db.Integer, nullable=False)
 
     # Created by and modified by
-    create_user = db.Column(db.String(50))
-    modified_user = db.Column(db.String(50))
+    create_user = db.Column(db.String(50), nullable=False)
+    modified_user = db.Column(db.String(50), nullable=True)
 
     # Timestamps
     # Created time and modified time
     create_time = db.Column(db.DateTime, default=datetime.now, nullable=False)
     modified_time = db.Column(
-        db.DateTime, default=datetime.now, onupdate=datetime.now, nullable=False
+        db.DateTime, default=datetime.now, onupdate=datetime.now, nullable=True
     )
+    
+    # User status
+    is_locked = db.Column(db.Integer, default=1, nullable=False)
 
     # User deletion status
-    is_del = db.Column(db.Boolean, default=False, nullable=False)
+    is_del = db.Column(db.Boolean, default='0', nullable=False)
 
     def __repr__(self):
         return f"<SysUser {self.username}>"
