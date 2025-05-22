149f86359ec01cae5f4c97a268183ae5ef5e9388
KiritoTu <jxzhenhaitu@gmail.com>
2025-05-22 16:15:12 +0800

    Debug

--- wenxige-bg-service/src/main/java/com/wenxige/bg/service/impl/CategoryServiceImpl.java
+++ wenxige-bg-service/src/main/java/com/wenxige/bg/service/impl/CategoryServiceImpl.java
@@ -3,7 +3,6 @@ package com.wenxige.bg.service.impl;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.wenxige.bg.entity.Category;
 import com.wenxige.bg.mapper.CategoryMapper;
-import com.wenxige.bg.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -11,7 +10,7 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 @RequiredArgsConstructor
-public class CategoryServiceImpl extends ServiceImpl<CategoryMapper, Category> implements CategoryService {
+public class CategoryServiceImpl extends ServiceImpl<CategoryMapper, Category> {
 
     private final CategoryMapper categoryMapper;
 
